b0e1396da10a3e7d7d8b95af3b37a92292725bcc
Shroff, Chandana <SHROFC1@nationwide.com>
2018-10-17 15:19:24 -0400

    test

---
+++ src/main/java/com/learnathon/jr/pizzashopapi/PizzaRestController.java
@@ -0,0 +1,13 @@
+package com.learnathon.jr.pizzashopapi;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class PizzaRestController{
+
+    @RequestMapping("/pizza")
+    public String getPizzas(){
+        return "Pizzas are Good!!!";
+    }
+}
---
+++ src/main/java/com/learnathon/jr/pizzashopapi/PizzaShopApiApplication.java
@@ -0,0 +1,12 @@
+package com.learnathon.jr.pizzashopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaShopApiApplication.class, args);
+	}
+}
